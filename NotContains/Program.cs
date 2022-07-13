cc59c5e69ef750a44c8db2a8f89c0333aeb6ecc2
zuka <zuka@DESKTOP-01OQQMQ>
2022-07-13 21:34:32 +0400

    Add project files.

---
+++ NotContains/Program.cs
@@ -0,0 +1,11 @@
+﻿static int NotContains(int[] arr)
+{
+    Array.Sort(arr);
+    int answer = 1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == answer)
+            answer++;
+    }
+    return answer;
+}
